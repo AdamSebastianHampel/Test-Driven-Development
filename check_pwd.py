6a5c3d389c9c2f75dff9927560960a015881adaf
Adam Hampel <hampela@oregonstate.edu>
2023-02-14 19:20:54 -0800

    Made check_pwd return False when a password with no uppercase letters is passed. Test passes.

--- check_pwd.py
+++ check_pwd.py
@@ -13,7 +13,13 @@ def check_pwd(pwd):
     if lower == 0:
         return False
     
-    if pwd == "1234567891ad":
+    upper = 0
+    upperStr = string.ascii_uppercase
+    for i in pwd:
+        if i in upperStr:
+            upper = 1
+            break
+    if upper == 0:
         return False
-    
+
     return True
