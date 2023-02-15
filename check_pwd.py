18dbf1114470241081354e1da1b6c5e209d03c8d
Adam Hampel <hampela@oregonstate.edu>
2023-02-14 18:42:20 -0800

    Made check_pwd return False when any string with no lowercase character is passed. Test passed.

--- check_pwd.py
+++ check_pwd.py
@@ -1,8 +1,16 @@
+import string
+
 def check_pwd(pwd):
     if len(pwd) < 8 or len(pwd) > 20:
         return False
     
-    if pwd == '123456789':
+    lower = 0
+    lowerStr = string.ascii_lowercase
+    for i in pwd:
+        if i in lowerStr:
+            lower = 1
+            break
+    if lower == 0:
         return False
     
     return True
